9476800b73df612301600c250bf593e96341a8e4
tejeshwaanand <tejeshwanov25@gmail.com>
2025-01-05 18:55:22 +0530

    backend cors

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,20 @@ const express= require('express')
 const app=express()
 const port = 5000
 const mongodb =require("./db")
+const cors = require('cors');
 mongodb();
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
-    next();
-});
+
+app.use(cors());
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// app.use((req,res,next)=>{
+//     res.setHeader("Access-Control-Allow-Origin","*");
+//     res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+//     next();
+// });
 
 
 app.get('/',(req,res)=>{
