606f8d585c24df06b4c91fae7aee36fd1a03e04d
tejeshwaanand <tejeshwanov25@gmail.com>
2025-01-05 16:30:30 +0530

    backend update

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mongodb =require("./db")
 mongodb();
 
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
     next();
 });
